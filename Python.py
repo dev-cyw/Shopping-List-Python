1561ca73662e9aa4af83daf673f347b0b60373b2
dev-cyw <98186075+dev-cyw@users.noreply.github.com>
2022-07-19 20:23:52 +0100

    Update Python.py

--- Python.py
+++ Python.py
@@ -40,3 +40,6 @@ def OptionSelector():
         x = int(input("Do you want to continue \n1 for yes \n0 for no"))
 print(" (1) is to input a new item\n (2) is to see what item\n (3) is to show the amount of items\n (4) is to delete an item ")
 OptionSelector()
+txtfile = open('ShoppingList.txt', 'w')
+txtfile.write(str(shopList))
+txtfile.close()
